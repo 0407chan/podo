01c2f8171166a33f47f62f87eed71a4e99489a5d
chanho <0407chan@naver.com>
2025-07-29 23:56:15 +0900

    init proejct.
    - React, TypeScript, and Vite 기반의 새로운 프로젝트 초기화.
    - ESLint 및 Tauri 설정 포함, 기본적인 파일 구조와 의존성 추가.

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+});
