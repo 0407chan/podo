d3f1b4824011de238659e665ee3e3eb827c6c763
chanho <0407chan@naver.com>
2025-08-18 17:06:52 +0900

    feat: 의존성 추가 및 Vite 설정 개선
    - bun.lock 및 package.json 파일에 여러 패키지(@ant-design/icons, @tanstack/react-query 등)를 추가하여 기능 확장을 지원함.
    - Vite 설정 파일에서 tsconfigPaths 플러그인을 추가하여 TypeScript 경로 설정을 개선함.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // 1. prevent vite from obscuring rust errors
+  plugins: [react(), tsconfigPaths()],
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 5173,
     strictPort: true,
